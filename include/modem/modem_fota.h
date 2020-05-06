f68b6f4de78287bc4aa5296c245be90e61aec125
Tommi Kangas <tommi.kangas@gmail.com>
2020-05-06 13:56:48 +0300

    Modem FOTA library interface first version.

---
+++ include/modem/modem_fota.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+
+#ifndef MODEM_FOTA_H_
+#define MODEM_FOTA_H_
+
+/**
+ * @file modem_fota.h
+ *
+ * @defgroup modem_fota Modem FOTA client
+ *
+ * @{
+ *
+ * @brief Public APIs for the modem FOTA client.
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// TODO: Add comment
+int modem_fota_init(void);
+
+/** @} */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* MODEM_FOTA_H_ */
