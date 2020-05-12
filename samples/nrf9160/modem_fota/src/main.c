39cfd7e2b4efa15d7d169502bece2075b398ab81
Tommi Kangas <tommi.kangas@gmail.com>
2020-05-12 11:12:20 +0300

    Added basic FOTA support.

--- samples/nrf9160/modem_fota/src/main.c
+++ samples/nrf9160/modem_fota/src/main.c
@@ -5,9 +5,14 @@
  */
 
 #include <stdio.h>
+#include <bsd.h>
 #include <string.h>
 #include <zephyr.h>
+#include <power/reboot.h>
+#include <modem/bsdlib.h>
 #include <modem/lte_lc.h>
+#include <modem/at_cmd.h>
+#include <modem/at_notif.h>
 #include <modem/modem_fota.h>
 
 /**@brief Recoverable BSD library error. */
@@ -18,12 +23,51 @@ void bsd_recoverable_error_handler(uint32_t err)
 
 void main(void)
 {
+	int err;
+
 	printk("Modem FOTA sample started\n");
+
+	printk("Initializing bsdlib...\n");
+	err = bsdlib_init();
+	switch (err) {
+	case MODEM_DFU_RESULT_OK:
+		printk("Modem firmware update successful!\n");
+		printk("Modem will run the new firmware after reboot\n");
+		sys_reboot(SYS_REBOOT_WARM);
+		break;
+	case MODEM_DFU_RESULT_UUID_ERROR:
+	case MODEM_DFU_RESULT_AUTH_ERROR:
+		printk("Modem firmware update failed!\n");
+		printk("Modem will run non-updated firmware on reboot.\n");
+		// TODO: Is reboot the correct thing to do?
+		sys_reboot(SYS_REBOOT_WARM);
+		break;
+	case MODEM_DFU_RESULT_HARDWARE_ERROR:
+	case MODEM_DFU_RESULT_INTERNAL_ERROR:
+		printk("Modem firmware update failed!\n");
+		printk("Fatal error.\n");
+		// TODO: Is reboot the correct thing to do?
+		sys_reboot(SYS_REBOOT_WARM);
+		break;
+	case -1:
+		printk("Could not initialize bsdlib.\n");
+		printk("Fatal error.\n");
+		return;
+	default:
+		break;
+	}
+	printk("Initialized bsdlib\n");
+
+	// Initialize AT command and notification libraries because
+	// CONFIG_BSD_LIBRARY_SYS_INIT is disabled and these libraries aren't
+	// initialized automatically
+	at_cmd_init();
+	at_notif_init();
 
 	modem_fota_init();
 
 	printk("LTE link connecting...\n");
-	int err = lte_lc_init_and_connect();
+	err = lte_lc_init_and_connect();
 	__ASSERT(err == 0, "LTE link could not be established.");
 	printk("LTE link connected!\n");
 
