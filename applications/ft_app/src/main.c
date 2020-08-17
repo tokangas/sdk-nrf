a333d6ebd85317e2f05be824cd8eeb84a094139a
Rantanen <Tommi.Rantanen@nordicsemi.no>
2020-08-17 15:47:25 +0300

    Enable modem traces

--- applications/ft_app/src/main.c
+++ applications/ft_app/src/main.c
@@ -8,7 +8,44 @@
 #include <stdio.h>
 #include <string.h>
 
+#include <nrf9160.h>
+#include <hal/nrf_gpio.h>
+
+static void modem_trace_enable(void)
+{
+	/* GPIO configurations for trace and debug */
+	#define CS_PIN_CFG_TRACE_CLK	21 //GPIO_OUT_PIN21_Pos
+	#define CS_PIN_CFG_TRACE_DATA0	22 //GPIO_OUT_PIN22_Pos
+	#define CS_PIN_CFG_TRACE_DATA1	23 //GPIO_OUT_PIN23_Pos
+	#define CS_PIN_CFG_TRACE_DATA2	24 //GPIO_OUT_PIN24_Pos
+	#define CS_PIN_CFG_TRACE_DATA3	25 //GPIO_OUT_PIN25_Pos
+
+	// Configure outputs.
+	// CS_PIN_CFG_TRACE_CLK
+	NRF_P0_NS->PIN_CNF[CS_PIN_CFG_TRACE_CLK] = (GPIO_PIN_CNF_DRIVE_H0H1 << GPIO_PIN_CNF_DRIVE_Pos) |
+		(GPIO_PIN_CNF_INPUT_Disconnect << GPIO_PIN_CNF_INPUT_Pos);
+
+	// CS_PIN_CFG_TRACE_DATA0
+	NRF_P0_NS->PIN_CNF[CS_PIN_CFG_TRACE_DATA0] = (GPIO_PIN_CNF_DRIVE_H0H1 << GPIO_PIN_CNF_DRIVE_Pos) |
+		(GPIO_PIN_CNF_INPUT_Disconnect << GPIO_PIN_CNF_INPUT_Pos);
+
+	// CS_PIN_CFG_TRACE_DATA1
+	NRF_P0_NS->PIN_CNF[CS_PIN_CFG_TRACE_DATA1] = (GPIO_PIN_CNF_DRIVE_H0H1 << GPIO_PIN_CNF_DRIVE_Pos) |
+		(GPIO_PIN_CNF_INPUT_Disconnect << GPIO_PIN_CNF_INPUT_Pos);
+
+	// CS_PIN_CFG_TRACE_DATA2
+	NRF_P0_NS->PIN_CNF[CS_PIN_CFG_TRACE_DATA2] = (GPIO_PIN_CNF_DRIVE_H0H1 << GPIO_PIN_CNF_DRIVE_Pos) |
+		(GPIO_PIN_CNF_INPUT_Disconnect << GPIO_PIN_CNF_INPUT_Pos);
+
+	// CS_PIN_CFG_TRACE_DATA3
+	NRF_P0_NS->PIN_CNF[CS_PIN_CFG_TRACE_DATA3] = (GPIO_PIN_CNF_DRIVE_H0H1 << GPIO_PIN_CNF_DRIVE_Pos) |
+		(GPIO_PIN_CNF_INPUT_Disconnect << GPIO_PIN_CNF_INPUT_Pos);
+
+	NRF_P0_NS->DIR = 0xFFFFFFFF;
+}
+
 void main(void)
 {
 	printk("The FT host sample started\n");
+	modem_trace_enable();
 }
