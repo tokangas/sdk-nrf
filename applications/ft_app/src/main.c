49f4c59bf5b0a47c3f1cb84b511b01fed605f793
Rantanen <Tommi.Rantanen@nordicsemi.no>
2020-08-17 09:44:02 +0300

    Initial FT app

---
+++ applications/ft_app/src/main.c
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2018 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+
+#include <zephyr.h>
+#include <stdio.h>
+#include <string.h>
+
+void main(void)
+{
+	printk("The FT host sample started\n");
+}
