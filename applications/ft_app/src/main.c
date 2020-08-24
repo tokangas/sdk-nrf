02fc12c4d3ae198ef0436f3d1aaf1133e6e4e3e2
Hirsimäki <Jani.Hirsimaki@nordicsemi.no>
2020-08-24 09:43:06 +0300

    Printing operator and APN when connected

--- applications/ft_app/src/main.c
+++ applications/ft_app/src/main.c
@@ -26,53 +26,78 @@ char rsp_buf[CONFIG_AT_CMD_RESPONSE_MAX_LEN];
 
 static void modem_trace_enable(void)
 {
-	/* GPIO configurations for trace and debug */
-	#define CS_PIN_CFG_TRACE_CLK	21 //GPIO_OUT_PIN21_Pos
-	#define CS_PIN_CFG_TRACE_DATA0	22 //GPIO_OUT_PIN22_Pos
-	#define CS_PIN_CFG_TRACE_DATA1	23 //GPIO_OUT_PIN23_Pos
-	#define CS_PIN_CFG_TRACE_DATA2	24 //GPIO_OUT_PIN24_Pos
-	#define CS_PIN_CFG_TRACE_DATA3	25 //GPIO_OUT_PIN25_Pos
+/* GPIO configurations for trace and debug */
+#define CS_PIN_CFG_TRACE_CLK 21 //GPIO_OUT_PIN21_Pos
+#define CS_PIN_CFG_TRACE_DATA0 22 //GPIO_OUT_PIN22_Pos
+#define CS_PIN_CFG_TRACE_DATA1 23 //GPIO_OUT_PIN23_Pos
+#define CS_PIN_CFG_TRACE_DATA2 24 //GPIO_OUT_PIN24_Pos
+#define CS_PIN_CFG_TRACE_DATA3 25 //GPIO_OUT_PIN25_Pos
 
 	// Configure outputs.
 	// CS_PIN_CFG_TRACE_CLK
-	NRF_P0_NS->PIN_CNF[CS_PIN_CFG_TRACE_CLK] = (GPIO_PIN_CNF_DRIVE_H0H1 << GPIO_PIN_CNF_DRIVE_Pos) |
+	NRF_P0_NS->PIN_CNF[CS_PIN_CFG_TRACE_CLK] =
+		(GPIO_PIN_CNF_DRIVE_H0H1 << GPIO_PIN_CNF_DRIVE_Pos) |
 		(GPIO_PIN_CNF_INPUT_Disconnect << GPIO_PIN_CNF_INPUT_Pos);
 
 	// CS_PIN_CFG_TRACE_DATA0
-	NRF_P0_NS->PIN_CNF[CS_PIN_CFG_TRACE_DATA0] = (GPIO_PIN_CNF_DRIVE_H0H1 << GPIO_PIN_CNF_DRIVE_Pos) |
+	NRF_P0_NS->PIN_CNF[CS_PIN_CFG_TRACE_DATA0] =
+		(GPIO_PIN_CNF_DRIVE_H0H1 << GPIO_PIN_CNF_DRIVE_Pos) |
 		(GPIO_PIN_CNF_INPUT_Disconnect << GPIO_PIN_CNF_INPUT_Pos);
 
 	// CS_PIN_CFG_TRACE_DATA1
-	NRF_P0_NS->PIN_CNF[CS_PIN_CFG_TRACE_DATA1] = (GPIO_PIN_CNF_DRIVE_H0H1 << GPIO_PIN_CNF_DRIVE_Pos) |
+	NRF_P0_NS->PIN_CNF[CS_PIN_CFG_TRACE_DATA1] =
+		(GPIO_PIN_CNF_DRIVE_H0H1 << GPIO_PIN_CNF_DRIVE_Pos) |
 		(GPIO_PIN_CNF_INPUT_Disconnect << GPIO_PIN_CNF_INPUT_Pos);
 
 	// CS_PIN_CFG_TRACE_DATA2
-	NRF_P0_NS->PIN_CNF[CS_PIN_CFG_TRACE_DATA2] = (GPIO_PIN_CNF_DRIVE_H0H1 << GPIO_PIN_CNF_DRIVE_Pos) |
+	NRF_P0_NS->PIN_CNF[CS_PIN_CFG_TRACE_DATA2] =
+		(GPIO_PIN_CNF_DRIVE_H0H1 << GPIO_PIN_CNF_DRIVE_Pos) |
 		(GPIO_PIN_CNF_INPUT_Disconnect << GPIO_PIN_CNF_INPUT_Pos);
 
 	// CS_PIN_CFG_TRACE_DATA3
-	NRF_P0_NS->PIN_CNF[CS_PIN_CFG_TRACE_DATA3] = (GPIO_PIN_CNF_DRIVE_H0H1 << GPIO_PIN_CNF_DRIVE_Pos) |
+	NRF_P0_NS->PIN_CNF[CS_PIN_CFG_TRACE_DATA3] =
+		(GPIO_PIN_CNF_DRIVE_H0H1 << GPIO_PIN_CNF_DRIVE_Pos) |
 		(GPIO_PIN_CNF_INPUT_Disconnect << GPIO_PIN_CNF_INPUT_Pos);
 
 	NRF_P0_NS->DIR = 0xFFFFFFFF;
 }
 static void lte_handler(const struct lte_lc_evt *const evt)
 {
+	int ret;
+
 	switch (evt->type) {
 	case LTE_LC_EVT_NW_REG_STATUS:
 		if ((evt->nw_reg_status != LTE_LC_NW_REG_REGISTERED_HOME) &&
-		     (evt->nw_reg_status != LTE_LC_NW_REG_REGISTERED_ROAMING)) {
+		    (evt->nw_reg_status != LTE_LC_NW_REG_REGISTERED_ROAMING)) {
 			break;
 		}
 
 		printk("\nNetwork registration status: %s",
-			evt->nw_reg_status == LTE_LC_NW_REG_REGISTERED_HOME ?
-			"Connected - home network" : "Connected - roaming\n");
-        shell_execute_cmd(shell_backend_uart_get_ptr(), "ft");
+		       evt->nw_reg_status == LTE_LC_NW_REG_REGISTERED_HOME ?
+			       "Connected - home network" :
+			       "Connected - roaming\n");
+
+#if defined(CONFIG_MODEM_INFO)
+		char info_str[MODEM_INFO_MAX_RESPONSE_SIZE];
+		ret = modem_info_string_get(MODEM_INFO_OPERATOR, info_str,
+					    sizeof(info_str));
+		if (ret >= 0) {
+			printk("Operator: %s\n", info_str);
+		} else {
+			printk("\nUnable to obtain modem parameters (%d)", ret);
+		}
+		ret = modem_info_string_get(MODEM_INFO_APN, info_str,
+					    sizeof(info_str));
+		if (ret >= 0) {
+			printk("APN: %s\n", info_str);
+		} else {
+			printk("\nUnable to obtain modem parameters (%d)", ret);
+		}
+#endif
 		break;
 	case LTE_LC_EVT_CELL_UPDATE:
 		printk("\nLTE cell changed: Cell ID: %d, Tracking area: %d\n",
-			evt->cell.id, evt->cell.tac);
+		       evt->cell.id, evt->cell.tac);
 		break;
 	default:
 		break;
@@ -81,9 +106,9 @@ static void lte_handler(const struct lte_lc_evt *const evt)
 static int fta_shell_init(struct device *unused)
 {
 	ARG_UNUSED(unused);
-   
+
 	printk("\nThe FT app sample started\n\n");
-	
+
 	//shell_print(shell_backend_uart_get_ptr(), "ei toimi jos ei autoconnect");
 
 	return 0;
@@ -92,7 +117,6 @@ static int fta_shell_init(struct device *unused)
 void main(void)
 {
 	int err;
-
 #if defined(CONFIG_BSD_LIBRARY)
 	if (IS_ENABLED(CONFIG_LTE_AUTO_INIT_AND_CONNECT)) {
 		/* Do nothing, modem is already configured and LTE connected. */
@@ -100,7 +124,7 @@ void main(void)
 		err = lte_lc_init_and_connect_async(lte_handler);
 		if (err) {
 			printk("\nModem could not be configured, error: %d",
-				err);
+			       err);
 			return;
 		}
 
