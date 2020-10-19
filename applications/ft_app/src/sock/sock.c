3d4cc50765d1edad35f236abbfd3b3c50550e948
Rantanen <Tommi.Rantanen@nordicsemi.no>
2020-10-19 14:38:59 +0300

    Sock: Refactoring socket id validation from shell to sock #2

--- applications/ft_app/src/sock/sock.c
+++ applications/ft_app/src/sock/sock.c
@@ -41,6 +41,29 @@ typedef enum {
 	SOCKET_CMD_HELP
 } socket_command;
 
+struct data_transfer_info {
+	struct k_work work;
+	struct k_timer timer;
+	int socket_id;
+};
+
+typedef struct {
+	int id;
+	int fd;
+	int family;
+	int type;
+	int port;
+	int bind_port;
+	int pdn_cid;
+	bool in_use;
+	bool log_receive_data;
+	int64_t recv_start_time_ms;
+	int64_t recv_end_time_ms;
+	uint32_t recv_data_len;
+	bool recv_start_throughput;
+	struct addrinfo *addrinfo;
+	struct data_transfer_info send_info;
+} socket_info_t;
 
 #define SOCKET_ID_NONE -1
 #define SOCKET_FD_NONE -1
