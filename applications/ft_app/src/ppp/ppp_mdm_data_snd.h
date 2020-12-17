9748a106c93cc0bc582074882c42e75b9b173589
Jani Hirsimaki <jani.hirsimaki@nordicsemi.no>
2020-12-17 05:49:52 +0200

    mosh: ppp re-structurings

---
+++ applications/ft_app/src/ppp/ppp_mdm_data_snd.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2020 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+
+#ifndef PPP_SHELL_H
+#define PPP_SHELL_H
+
+#if defined (CONFIG_FTA_PPP)
+void ppp_mdm_data_snd_init();
+#endif
+
+#endif /* PPP_SHELL_H */
