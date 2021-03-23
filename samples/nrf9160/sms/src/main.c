3ef8d3d693e2f413ccce4cf606f6e53a131f14c1
Tommi Rantanen <tommi.rantanen@nordicsemi.no>
2021-03-24 00:20:04 +0200

    samples:sms: Added initial version of SMS sample.
    Signed-off-by: Tommi Rantanen <tommi.rantanen@nordicsemi.no>

---
+++ samples/nrf9160/sms/src/main.c
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2021 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+
+#include <stdio.h>
+#include <kernel.h>
+#include <string.h>
+#include <modem/sms.h>
+
+
+static void sms_callback(struct sms_data *const data, void *context)
+{
+	if (data == NULL) {
+		printk("sms_callback with NULL data\n");
+	}
+
+	if (data->type == SMS_TYPE_DELIVER) {
+		/* When SMS message is received, print information */
+		struct sms_deliver_header *header = &data->header.deliver;
+		printk("\nSMS message received:\n");
+		printk("\tTime:   %02d-%02d-%02d %02d:%02d:%02d\n",
+			header->time.year,
+			header->time.month,
+			header->time.day,
+			header->time.hour,
+			header->time.minute,
+			header->time.second);
+
+		printk("\tText:   '%s'\n", data->data);
+		printk("\tLength: %d\n", data->data_len);
+
+		if (header->app_port.present) {
+			printk("\tApplication port addressing scheme: dest_port=%d, src_port=%d\n",
+				header->app_port.dest_port,
+				header->app_port.src_port);
+		}
+		if (header->concatenated.present) {
+			printk("\tConcatenated short message: ref_number=%d, msg %d/%d\n",
+				header->concatenated.ref_number,
+				header->concatenated.seq_number,
+				header->concatenated.total_msgs);
+		}
+	} else if (data->type == SMS_TYPE_SUBMIT_REPORT) {
+		printk("SMS submit report received\n");
+		return;
+	} else {
+		printk("SMS protocol message with unknown type received\n");
+	}
+
+}
+
+void main(void)
+{
+	printk("\nSMS sample starting\n");
+
+	printk("\nRegistering SMS\n");
+	int handle = sms_register_listener(sms_callback, NULL);
+	if (handle) {
+		printk("sms_register_listener returned err: %d\n", handle);
+		return;
+	}
+
+	/* SMS sending is commented out here as destination phone number that
+	 * we should use is unknown. User can tweak the code, e.g., to send
+	 * the message to his/her personal phone.
+	 */
+	printk("\nHere you can send an SMS. Please find this line from the code.\n"
+		"Then, uncomment next line and change your phone number in there.\n");
+	/*
+	int ret = sms_send("000000000000", "SMS sample: testing"); 
+	if (ret) {
+		printk("sms_send returned err: %d\n", ret);
+	}
+	*/
+
+	/* In our application, we should unregister SMS in some conditions with:
+	 *   sms_unregister_listener(handle);
+	 * However, this sample will continue to be registered for
+	 * received SMS messages.
+	 */
+}
