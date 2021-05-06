c45b2923e5955f8d865fe3ebbe46629149d93312
Tommi Rantanen <Tommi.Rantanen@nordicsemi.no>
2021-05-06 14:07:49 +0300

    Make private function for modem trace enable.
    Signed-off-by: Tommi Rantanen <tommi.rantanen@nordicsemi.no>

--- samples/nrf9160/modem_shell/src/main.c
+++ samples/nrf9160/modem_shell/src/main.c
@@ -24,6 +24,8 @@
 #include <modem/modem_info.h>
 #include <modem/lte_lc.h>
 
+#include "private.h"
+
 #if defined(CONFIG_MOSH_PPP)
 #include <shell/shell.h>
 #include "ppp_ctrl.h"
@@ -63,44 +65,6 @@ void k_sys_fatal_error_handler(unsigned int reason,
 #endif
 #endif
 
-static void modem_trace_enable(void)
-{
-/* GPIO configurations for trace and debug */
-#define CS_PIN_CFG_TRACE_CLK 21 //GPIO_OUT_PIN21_Pos
-#define CS_PIN_CFG_TRACE_DATA0 22 //GPIO_OUT_PIN22_Pos
-#define CS_PIN_CFG_TRACE_DATA1 23 //GPIO_OUT_PIN23_Pos
-#define CS_PIN_CFG_TRACE_DATA2 24 //GPIO_OUT_PIN24_Pos
-#define CS_PIN_CFG_TRACE_DATA3 25 //GPIO_OUT_PIN25_Pos
-
-	// Configure outputs.
-	// CS_PIN_CFG_TRACE_CLK
-	NRF_P0_NS->PIN_CNF[CS_PIN_CFG_TRACE_CLK] =
-		(GPIO_PIN_CNF_DRIVE_H0H1 << GPIO_PIN_CNF_DRIVE_Pos) |
-		(GPIO_PIN_CNF_INPUT_Disconnect << GPIO_PIN_CNF_INPUT_Pos);
-
-	// CS_PIN_CFG_TRACE_DATA0
-	NRF_P0_NS->PIN_CNF[CS_PIN_CFG_TRACE_DATA0] =
-		(GPIO_PIN_CNF_DRIVE_H0H1 << GPIO_PIN_CNF_DRIVE_Pos) |
-		(GPIO_PIN_CNF_INPUT_Disconnect << GPIO_PIN_CNF_INPUT_Pos);
-
-	// CS_PIN_CFG_TRACE_DATA1
-	NRF_P0_NS->PIN_CNF[CS_PIN_CFG_TRACE_DATA1] =
-		(GPIO_PIN_CNF_DRIVE_H0H1 << GPIO_PIN_CNF_DRIVE_Pos) |
-		(GPIO_PIN_CNF_INPUT_Disconnect << GPIO_PIN_CNF_INPUT_Pos);
-
-	// CS_PIN_CFG_TRACE_DATA2
-	NRF_P0_NS->PIN_CNF[CS_PIN_CFG_TRACE_DATA2] =
-		(GPIO_PIN_CNF_DRIVE_H0H1 << GPIO_PIN_CNF_DRIVE_Pos) |
-		(GPIO_PIN_CNF_INPUT_Disconnect << GPIO_PIN_CNF_INPUT_Pos);
-
-	// CS_PIN_CFG_TRACE_DATA3
-	NRF_P0_NS->PIN_CNF[CS_PIN_CFG_TRACE_DATA3] =
-		(GPIO_PIN_CNF_DRIVE_H0H1 << GPIO_PIN_CNF_DRIVE_Pos) |
-		(GPIO_PIN_CNF_INPUT_Disconnect << GPIO_PIN_CNF_INPUT_Pos);
-
-	NRF_P0_NS->DIR = 0xFFFFFFFF;
-}
-
 static void mosh_print_version_info(void)
 {
 #if defined(APP_VERSION)
@@ -128,7 +92,8 @@ void main(void)
 
 	mosh_print_version_info();
 
-	modem_trace_enable();
+	/* Initialize private parts of the application */
+	private_initialization();
 
 #if !defined(CONFIG_LWM2M_CARRIER)
 	printk("Initializing modemlib...\n");
---
+++ samples/nrf9160/modem_shell/src/utils/private.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2021 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#ifndef _PRIVATE_INCLUDE_H_
+#define _PRIVATE_INCLUDE_H_
+
+/** @brief Initialize private parts of the application.
+ * 
+ * @details Replacing the content of this function enables confidential initializations.
+ */
+void private_initialization(void);
+
+#endif /* _PRIVATE_INCLUDE_H_ */
