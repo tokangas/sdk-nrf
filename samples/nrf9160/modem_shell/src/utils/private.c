5fa6709c1fa607c4b88e49cb09032c9ee0d0c7a0
Tommi Rantanen <Tommi.Rantanen@nordicsemi.no>
2021-05-06 16:07:25 +0300

    mosh: Remove modem_trace_enable()
    Signed-off-by: Tommi Rantanen <tommi.rantanen@nordicsemi.no>

--- samples/nrf9160/modem_shell/src/utils/private.c
+++ samples/nrf9160/modem_shell/src/utils/private.c
@@ -4,48 +4,7 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
 
-#include <hal/nrf_gpio.h>
-
-
-static void modem_trace_enable(void)
-{
-/* GPIO configurations for trace and debug */
-#define CS_PIN_CFG_TRACE_CLK 21 //GPIO_OUT_PIN21_Pos
-#define CS_PIN_CFG_TRACE_DATA0 22 //GPIO_OUT_PIN22_Pos
-#define CS_PIN_CFG_TRACE_DATA1 23 //GPIO_OUT_PIN23_Pos
-#define CS_PIN_CFG_TRACE_DATA2 24 //GPIO_OUT_PIN24_Pos
-#define CS_PIN_CFG_TRACE_DATA3 25 //GPIO_OUT_PIN25_Pos
-
-	// Configure outputs.
-	// CS_PIN_CFG_TRACE_CLK
-	NRF_P0_NS->PIN_CNF[CS_PIN_CFG_TRACE_CLK] =
-		(GPIO_PIN_CNF_DRIVE_H0H1 << GPIO_PIN_CNF_DRIVE_Pos) |
-		(GPIO_PIN_CNF_INPUT_Disconnect << GPIO_PIN_CNF_INPUT_Pos);
-
-	// CS_PIN_CFG_TRACE_DATA0
-	NRF_P0_NS->PIN_CNF[CS_PIN_CFG_TRACE_DATA0] =
-		(GPIO_PIN_CNF_DRIVE_H0H1 << GPIO_PIN_CNF_DRIVE_Pos) |
-		(GPIO_PIN_CNF_INPUT_Disconnect << GPIO_PIN_CNF_INPUT_Pos);
-
-	// CS_PIN_CFG_TRACE_DATA1
-	NRF_P0_NS->PIN_CNF[CS_PIN_CFG_TRACE_DATA1] =
-		(GPIO_PIN_CNF_DRIVE_H0H1 << GPIO_PIN_CNF_DRIVE_Pos) |
-		(GPIO_PIN_CNF_INPUT_Disconnect << GPIO_PIN_CNF_INPUT_Pos);
-
-	// CS_PIN_CFG_TRACE_DATA2
-	NRF_P0_NS->PIN_CNF[CS_PIN_CFG_TRACE_DATA2] =
-		(GPIO_PIN_CNF_DRIVE_H0H1 << GPIO_PIN_CNF_DRIVE_Pos) |
-		(GPIO_PIN_CNF_INPUT_Disconnect << GPIO_PIN_CNF_INPUT_Pos);
-
-	// CS_PIN_CFG_TRACE_DATA3
-	NRF_P0_NS->PIN_CNF[CS_PIN_CFG_TRACE_DATA3] =
-		(GPIO_PIN_CNF_DRIVE_H0H1 << GPIO_PIN_CNF_DRIVE_Pos) |
-		(GPIO_PIN_CNF_INPUT_Disconnect << GPIO_PIN_CNF_INPUT_Pos);
-
-	NRF_P0_NS->DIR = 0xFFFFFFFF;
-}
-
 void private_initialization(void)
 {
-	modem_trace_enable();
+	/* Add here your private initialization code */
 }
